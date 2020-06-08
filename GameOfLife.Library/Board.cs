0dbf76c941aedafbfdf15a2b7c19707b9e33dba1
ChrisTorng <christorng@hotmail.com>
2020-06-08 12:10:39 +0800

    Add Board with contructor. Add LibraryTests and BoardTests.

---
+++ GameOfLife.Library/Board.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace GameOfLife.Library
+{
+    public class Board
+    {
+        public BitArray[] Rows { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public Board(int width, int height)
+        {
+            if (width <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(width));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(height));
+            }
+
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+}
---
+++ GameOfLife.LibraryTests/BoardTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife.Library.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void Constructor_Valid_Test()
+        {
+            // Arrange
+            var board = new Board(10, 20);
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(10, board.Width);
+            Assert.AreEqual(20, board.Height);
+        }
+
+        [TestMethod]
+        public void Constructor_InvalidWidth_Test()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                new Board(0, 20));
+        }
+
+        [TestMethod]
+        public void Constructor_InvalidHeight_Test()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                new Board(10, 0));
+        }
+    }
+}
